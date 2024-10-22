ec9ae551a5f7eacd7466df1a1ce4ef30639ffcc7
Ernesto2322 <75189068@continental.edu.pe>
2024-10-22 14:37:08 -0500

    Creando el esqueleto de las clases Desviacón y TestDesviación

---
+++ src/logica/NoSePuedeCalcularDesviacion.py
@@ -0,0 +1,7 @@
+import math
+
+
+class NoSePuedeCalcularDesviacion(Exception):
+    """Excepción lanzada cuando no se puede calcular la desviación estándar."""
+    pass
+
---
+++ tests/desviacion.py
@@ -0,0 +1,5 @@
+import unittest
+from desviacion import calcular_desviacion_estandar, NoSePuedeCalcularDesviacion
+
+if __name__ == '__main__':
+    unittest.main()
