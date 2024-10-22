48f7e507d8a448214365a8719b175fd1e374c274
Ernesto2322 <75189068@continental.edu.pe>
2024-10-22 14:21:23 -0500

    Caso 1: Sin elementos (Rojo)

--- tests/media.py
+++ tests/media.py
@@ -4,7 +4,7 @@ from media import calcular_media, NoSePuedeCalcular
 class TestCalcularMedia(unittest.TestCase):
 
     def test_lista_vacia(self):
-        """Debe lanzar NoSePuedeCalcular para lista vacía."""
+        """Debe lanzar NoSePuedeCalcular para lista vacía. """
         with self.assertRaises(NoSePuedeCalcular):
             calcular_media([])  # Intento de calcular media en lista vacía
 
